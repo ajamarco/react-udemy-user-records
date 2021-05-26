22b0ac306537c62681fa12eec9e2f10372ab603f
alexandre jamarco <alexandre.j@gmail.com>
2021-05-26 21:54:35 +0100

    added initial styles

--- src/components/users/AddUser.js
+++ src/components/users/AddUser.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React from "react";
+import Card from "../UI/Card";
+import './AddUser.css'
 
 export default function AddUser() {
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(e);
+    }
+
     return (
-        <form>
-            <label htmlFor="name">Name</label>
-            <input type="text" name="name" id="name" />
-            <label htmlFor="age">Name</label>
-            <input type="number" name="age" id="age" />
-            <button type="submit">Add User</button>
-        </form>
-    )
+        <Card className='input'>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name" id="name" />
+                <label htmlFor="age">Name</label>
+                <input type="number" name="age" id="age" />
+                <button type="submit">Add User</button>
+            </form>
+        </Card>
+    );
 }
